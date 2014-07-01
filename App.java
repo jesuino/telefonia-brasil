dc4f8655758e2c9157f0c3b62a3cdf8ddb66e6f2
jesuino <william.a.siqueira@gmail.com>
2014-07-01 20:39:29 -0300

    Adicionando arquivos iniciais

---
+++ App.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.input.MouseEvent;
+
+public class App extends Application {
+	
+
+	public void start(Stage s) throws Exception{
+		Scene cena = new Scene(FXMLLoader.load(getClass().getResource("./app.fxml")));
+		cena.getStylesheets().add("app.css");
+		s.setScene(cena);
+		s.show();
+		s.setTitle("Dados sobre telefonia no Brasil");
+	}
+	
+
+	public void estadoClicado(MouseEvent e){
+		System.out.println("Label Clicado!");
+	}
+	
+}
---
+++ run.sh
@@ -0,0 +1,4 @@
+JAVA_HOME=/opt/java/jdk1.8.0/
+rm *class
+$JAVA_HOME/bin/javac App.java
+$JAVA_HOME/bin/java App
